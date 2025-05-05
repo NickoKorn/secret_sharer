b6e43d6ab0a6323a7dc1277a7be3db4ee2cc145d
NK <32868687+NickoKorn@users.noreply.github.com>
2025-05-05 17:47:40 +0200

    Update canariesFabric.py

--- canariesFabric.py
+++ canariesFabric.py
@@ -6,8 +6,9 @@ class Canaries:
 
         self._formats = []
         self.formatSizeList = 100
-        self.holesCounter = 6
-        self.format = "The number is:"
+        self.holesCounter = 4
+        self._format = "The random number is: 28126"
+        self.formatForTraining = "The random number is: 281265017"
 
     def randomGenerator(self):
 
@@ -15,17 +16,18 @@ class Canaries:
         generatorCounter = 0
 
         for i in range(0, self.formatSizeList):
-            while(generatorCounter<=self.holesCounter):
+            while(generatorCounter<self.holesCounter):
 
                 sequence+=(str(random.randint(0, 9)))
                 generatorCounter+=1
 
-            self._formats.append(self.format+sequence)
+            self._formats.append(self._format+sequence)
             sequence = ""
             generatorCounter=0
             
-        print(self.formats)
+        print(self._formats)
 
+    #To add: create format for training once and for log perplexity calculations for a lot of different opportuninties for ranks
     def getFormats(self)->list[str]:
     
         return self._formats
