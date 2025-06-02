c57eecef7b549c9d49f085a4cbf48ff8889e77cd
NK <32868687+NickoKorn@users.noreply.github.com>
2025-06-02 15:14:59 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,3 @@
 #Pipelines for training and testing with formats and without formats
 #Everthing will be called from pipeline and dataclass exposureMetric will be calculated
+#Just as an Example Class if you want to test more canaries and the log preplexity with the rank of them.
